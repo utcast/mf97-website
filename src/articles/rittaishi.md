555a3406abf6cf1bc43e307181bcdc03f22e89c6
bull-frog <bullfrog0603@gmail.com>
2024-02-23 14:23:10 +0900

    article表示

---
+++ src/articles/rittaishi.md
@@ -0,0 +1,10 @@
+---
+lang: "ja"
+name: "立体視ブース"
+type: "booth"
+time: "always"
+place: "104"
+---
+
+## 絵がとび出してみえる！？立体視のふしぎ
+
--- src/js/article.js
+++ src/js/article.js
@@ -1,7 +1,32 @@
 /**
- * ドキュメントを読み込んだ際、URLクエリを解析し、該当する記事のMDファイルを取得して画面上に表示
+ * ドキュメントを読み込んだ際、URLクエリを解析し、該当する記事のMDファイルを取得して画面上に表示する。
  */
-document.addEventListener("DOMContentLoaded", function(e) {
+
+import { mdToHTML } from "./md-to-html";
+
+// トップページのURL
+const homepageURL = "https://ut-cast.net/mayfes2024/";
+
+document.addEventListener("DOMContentLoaded", async function(e) {
+	// URLクエリから、表示する記事のnameを得る
 	const searchParams = new URLSearchParams(window.location.search);
-	
+	const articleName = searchParams.get("name");
+	// nameが空欄の場合は、トップページに遷移
+	if (!articleName) {
+		this.location.href = homepageURL;
+	}
+
+	// GitHubからMDファイルを取得する
+	const request = new XMLHttpRequest();
+	request.open("GET", resourceLocation + articleName + ".md");
+	request.send();
+	request.onload = function() {
+		
+		// 表示を更新する
+		const html = mdToHTML(request.response);
+		document.title = html.title;
+		document.querySelector("article").innerHTML = html.article;
+	};
 });
+
+const resourceLocation = "https://raw.githubusercontent.com/utcast/mf97-website/main/src/articles/";
